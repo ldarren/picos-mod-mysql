9ea1489c195de1ebb48fe6bc3ffae2c6536174f9
Darren Liew <ldarren@gmail.com>
2018-09-02 06:11:02 +0000

    updated test suite

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 const pico = require('pico-common/bin/pico-cli')
-const { ensure } = pico.export('pico/test')
+const { test } = pico.export('pico/test')
 const mysql = require('./index')
 
 let client
 
-ensure('ensure mysql loaded', function(cb){
+test('ensure mysql loaded', function(cb){
 	cb(null, !!mysql)
 })
-ensure('ensure mysql create', function(cb){
+test('ensure mysql create', function(cb){
 	mysql.create({ path: '', env: 'pro' }, {}, (err, cli) => {
 		if (err) return cb(err)
 		client = cli
